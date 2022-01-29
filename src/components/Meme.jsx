a7da3aa242e221dcca02c9ebbaca8f7d390b6985
Rohit Singh <rohit14kvs@gmail.com>
2022-01-29 17:46:27 +0530

    Ui done

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import memeData from "../../src/memeData";
 
 const Meme = () => {
+  const memeArray = memeData.data.memes;
+  const randomNumber = Math.floor(Math.random() * memeArray.length);
+  const [memeImage, setmemeImage] = useState("");
+
+  const getMemeImage = () => {
+    setmemeImage(memeArray[randomNumber].url);
+    console.log(memeArray[randomNumber].url);
+  };
   return (
     <section>
       <div className="input__section">
-        <form className="form">
-          <input type="text" className="form__input" />
-          <input type="text" className="form__input" />
-          <button className="form__button">Get a new meme</button>
-        </form>
+        <div className="form">
+          <input type="text" placeholder="Top text" className="form__input" />
+          <input
+            type="text"
+            placeholder="Bottom text"
+            className="form__input"
+          />
+          <button className="form__button" onClick={getMemeImage}>
+            Get a new meme image
+          </button>
+        </div>
+        <div className="img__container">
+          {memeImage === "" ? (
+            <div className="silly__text">Click button to get an image</div>
+          ) : (
+            <img src={memeImage} alt="Meme" className="meme__img" />
+          )}
+        </div>
+        {memeImage && <button className="download">Download</button>}
       </div>
     </section>
   );
